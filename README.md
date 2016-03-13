832ffcb31e03eacf109b31b7ff1f0310b1c9979c
Mark Furneaux <mark@furneaux.ca>
2016-03-13 19:13:23 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+offline-zfs-backup
+==================
+
+This script works in tandem with a script like https://github.com/adaugherity/zfs-backup
+which performs automatic incremental snapshot send-receive to a server which is not always running.
+
+This script uses WOL to wake up the machine, do some preliminary checks, invoke the backup script, and poweroff the machine once again.
+
+Setup
+=====
+
+1. Setup a script to perform incremental backup such as [zfs-backup](https://github.com/adaugherity/zfs-backup).
+2. Setup your receiving server to wake via WOL.   
+This includes BIOS setup, and running: ```ethtool -s eth0 wol g``` after startup (```rc.local``` is a good place).
+3. Add the MAC address of the receiver, its IP address (if static), the login information, and the path to the script to run, into this script.
+4. Optionally add a sendmail command or similar to warn if the pool is degraded.
---
+++ run-zfs-backup.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#
+# ZFS Incremental Backup Init Script
+# Version 0.0.1
+# Copyright 2016 Mark Furneaux, Romaco Canada
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+WOL_MAC="aa:bb:cc:dd:ee"
+WOL_IP="192.168.30.145"
+REM_LOGIN="root@darwin"
+ZFS_BACKUP_SCRIPT="/tank/storage/Scripts/zfs-backup.sh"
+
+echo ""
+echo "Romaco ZFS Offline Backup Script v0.0.1 Starting..."
+echo "Start time: $(date)"
+echo ""
+
+# wake up backup server (3 times beacause I don't trust WOL)
+echo "Waking up backup server..."
+wakeonlan $WOL_MAC
+wakeonlan $WOL_MAC
+wakeonlan -i $WOL_IP $WOL_MAC
+echo ""
+
+# give the server some time to boot
+echo "Waiting 2 minutes for server to boot..."
+sleep 120
+
+# check if it's done booting
+echo "Checking if server is up..."
+ssh -n $REM_LOGIN "uptime"
+echo ""
+
+# if it's not ready, try again 3 times before giving up
+RETRIES=3
+until [ $? -eq 0 -o $RETRIES -eq 0 ]; do
+	echo "Server not ready. Trying again in another 30 seconds..."
+	let RETRIES-=1
+	sleep 30
+	ssh -n $REM_LOGIN "uptime"
+	echo ""
+done
+
+if [ $RETRIES -eq 0 ]; then
+	echo "ERROR: Backup server not online, giving up"
+	exit 1
+fi
+
+echo "Server is up"
+
+# check if the remote pool is healthy
+echo "Checking health of remote pool..."
+ssh -n $REM_LOGIN "zpool status -x btank"
+echo ""
+
+# notify and abort if there is a pool problem
+if [ $? -ne 0 ]; then
+	echo "ERROR: Pool not healthy, aborting"
+	echo "Problem with pool detected. Sending alert email..."
+	# TODO call your own sendmail program/script here
+	# shut down the backup server
+	echo "Powering off backup server..."
+	ssh -n $REM_LOGIN "poweroff"
+	exit 2
+fi
+
+# run the backup and log the output
+echo "All good, running backup..."
+$ZFS_BACKUP_SCRIPT
+
+if [ $? -eq 0 ]; then
+	echo "Backup complete"
+else
+	echo "ERROR: Backup script returned non-zero exit code: $?"
+fi
+
+echo "Preparing to poweroff server..."
+sleep 30
+
+# shut down the backup server
+echo "Powering off backup server..."
+ssh -n $REM_LOGIN "poweroff"
+echo "End time: $(date)"
+echo "Backup process complete. Exiting"
